93774e33fcc464404af02dfa9880c088f4d6c2dc
ibrahimahmedoglu <ibrahim.ahmedoglu@bahcesehir.edu.tr>
2023-06-30 13:50:43 +0300

    completed assigntment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const textElement = document.querySelector("#text");
+    textElement.textContent = "This is really cool!";
+  });
+  
